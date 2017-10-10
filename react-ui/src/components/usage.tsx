a0ffbab29d5e29481bf2fb642399e8dd3f85a93a
birdhut <birdhut@outlook.com>
2017-10-10 19:47:53 +0100

    Added SignalR working with output to the UI of the CPU Usage and Bytes Used

--- react-ui/src/components/usage.tsx
+++ react-ui/src/components/usage.tsx
@@ -1,43 +1,54 @@
 import * as React from 'react';
-// import * as $ from 'jquery';
+import { Well } from 'react-bootstrap';
 
-class Usage extends React.Component {
+class Usage extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            usage: { CpuPercent: 0, BytesUsed: 0 }
+        };
+    }
+
     proxy: SignalR.Hub.Proxy;
 
-    componentDidMount() {
-        // var handler = this.updateStockHandler;
-        // var allHandler = this.updateAllStocksHandler;
+    handleUpdateUsage = (u: any) => {
+        this.setState( {usage : u} );
+    }
 
-        // $.connection.hub.url = 'http://localhost:8080/signalr';
-        
-        // Add a client-side hub method that the server will call
+    componentDidMount() {
+
+        // Configure Connection
+        $.connection.hub.url = 'http://localhost:8080/signalr';
     
         // Turn on logging
-        // $.connection.hub.logging = true;
-    
+        $.connection.hub.logging = true;
+
+        // Create dynamic proxy for usageHub
+        this.proxy = $.connection.hub.createHubProxy('usageHub');
+        this.proxy.on('updateUsage', (usage) => {
+            this.handleUpdateUsage(usage);
+        });
+
         // Start the connection
-        // this.proxy = $.connection.hub.createHubProxy("usageHub");
-        // this.proxy.on("updateUsage", (usage: any) => {
-
-        // });
-
-        // var p = this.proxy;
-        // $.connection.hub.start().done(function() {
-        //     p.invoke("register").done(function(usage: any) {
-
-        //     });
-        // });
+        $.connection.hub.start().done(() => {
+            this.proxy.invoke('register').done((usage: any) => {
+                this.handleUpdateUsage(usage);    
+            });
+        });
     }
 
     componentWillUnMount() {
-        // this.proxy.off("updateUsage", (usage: any) => {
-            
-        // });
+        this.proxy.off('updateUsage', (usage: any) => {
+            this.setState({ usage: { CpuPercent: 0, BytesUsed: 0 } });
+        });
     }
 
     render() {
         return (
-            <div>Usage
+            <div>
+                <h2>Current Usage</h2>
+                <Well>CPU: {this.state.usage.CpuPercent * 100} %</Well>
+                <Well>RAM: {Math.round((this.state.usage.BytesUsed / 1024 / 1024) * 100) / 100} GB</Well>
             </div>
         );
     }
